8eed836e1fd3a06d9cf61534cd8d65c5d0051a61
BoomVisibility <web@boomvisibility.com>
2016-01-01 08:52:39 -0500

    remove spacing and upper footer cta

--- 404.php
+++ 404.php
@@ -13,19 +13,16 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
+					<h1 class="entry-title"><?php _e( '404 Page Not Found', 'twentytwelve' ); ?></h1>
 				</header>
 			<article id="post-0" class="post error404 no-results not-found">
-
 				<div class="entry-content">
 					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwelve' ); ?></p>
 					<?php get_search_form(); ?>
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
-
 		</div><!-- #content -->
 	</div><!-- #primary -->
 	<?php get_sidebar(); ?>
 </div>
-<?php if ( is_active_sidebar( 'sidebar-10' ) ) : ?><?php dynamic_sidebar( 'sidebar-10' ); ?><?php endif; ?>
 <?php get_footer(); ?>
