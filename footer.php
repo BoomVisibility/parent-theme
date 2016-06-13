c35badf92e1418671dbd728585c824ccf63a4c40
BoomVisibility <web@boomvisibility.com>
2016-06-13 11:40:44 -0400

    Change to footer columns
    Footer columns changed to one_third. Fourth column removed (not in functions.php)

--- footer.php
+++ footer.php
@@ -17,26 +17,21 @@
 	<footer class="global" role="contentinfo">
 	<div class="container">
 
-			<div class="one-fourth">
+			<div class="one_third">
 				<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
 					<?php dynamic_sidebar( 'sidebar-4' ); ?>
 				<?php endif; ?>
 			</div>
-			<div class="one-fourth">
+			<div class="one_third">
 				<?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
 					<?php dynamic_sidebar( 'sidebar-5' ); ?>
 				<?php endif; ?>
 			</div>
-			<div class="one-fourth">
+			<div class="one_third last">
 				<?php if ( is_active_sidebar( 'sidebar-6' ) ) : ?>
 					<?php dynamic_sidebar( 'sidebar-6' ); ?>
 				<?php endif; ?>
 				
-			</div>
-			<div class="one-fourth last">
-				<?php if ( is_active_sidebar( 'sidebar-9' ) ) : ?>
-					<?php dynamic_sidebar( 'sidebar-9' ); ?>
-				<?php endif; ?>
 			</div>
 			<div class="clear"></div>
 
