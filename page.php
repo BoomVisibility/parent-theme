1672f2c8a7c2723c8578090d53ca8898d299fdd5
BoomVisibility <web@boomvisibility.com>
2016-06-14 12:31:36 -0400

    Close header
    Header did not have a closing tag

--- page.php
+++ page.php
@@ -15,27 +15,23 @@
 get_header(); ?>
 <div class="header-divider"></div>
 <div class="wrapper">
-
 	<div id="primary" class="page-content">
 		<div id="content" class="container" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<header class="entry-header">
-			<h1 class="page-title"><?php the_title(); ?></h1>
-		
-		<?php if ( function_exists('yoast_breadcrumb') ) {
-			yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-			} ?>
+					<h1 class="page-title"><?php the_title(); ?></h1>
+				</header>
+				<?php if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+					} ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	
 	<?php if ( is_active_sidebar( 'sidebar-11' ) ) : ?>
 	<div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-11' ); ?>				
 	</div><!-- #secondary -->			
 	<?php endif; ?>	
-
 </div>
-		
 <?php get_footer(); ?>
