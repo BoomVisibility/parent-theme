89248e0cd83416787fa01ae837f12e44f40ee1d5
BoomVisibility <web@boomvisibility.com>
2016-08-29 11:05:54 -0400

    Header not closed
    Added close to header on line 27

--- page-templates/full-width.php
+++ page-templates/full-width.php
@@ -20,11 +20,11 @@ get_header(); ?>
 		<div id="content" class="container" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<header class="entry-header">
-			<h1 class="page-title"><?php the_title(); ?></h1>
-		
-		<?php if ( function_exists('yoast_breadcrumb') ) {
-			yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-			} ?>
+					<h1 class="page-title"><?php the_title(); ?></h1>
+					<?php if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+					} ?>
+				</header>
 				<?php get_template_part( 'content', 'page' ); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
