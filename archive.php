8c470650202798753bb8fdb43c48091a8872fce6
BoomVisibility <web@boomvisibility.com>
2017-12-07 15:42:14 -0500

    Update archive.php

--- archive.php
+++ archive.php
@@ -18,14 +18,15 @@
  */
 get_header(); ?>
 <header class="entry-header">				
-<h1 class="entry-title">
-<?php					
-if ( is_day() ) : printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
-elseif ( is_month() ) :	printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
-elseif ( is_year() ) : printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
-else : _e( 'Archives', 'twentytwelve' );
-endif;
-?></h1>
+	<h1 class="entry-title">
+		<?php					
+			if ( is_day() ) : printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
+			elseif ( is_month() ) :	printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
+			elseif ( is_year() ) : printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
+			else : _e( 'Archives', 'twentytwelve' );
+			endif;
+		?>
+	</h1>
 </header><!-- .entry-header -->
 <div class="wrapper archive-page">
 	<?php if ( have_posts() ) : ?>			
