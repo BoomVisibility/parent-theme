6e76758dc3f5bc8795424335b196510041b780cb
BoomVisibility <web@boomvisibility.com>
2018-05-15 12:54:17 -0400

    Add dashicons

--- functions.php
+++ functions.php
@@ -793,3 +793,9 @@ function wpbeginner_numeric_posts_nav() {
 	echo '</ul></div>' . "\n";
 
 }
+
+//Enqueue the Dashicons script
+add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
+function load_dashicons_front_end() {
+wp_enqueue_style( 'dashicons' );
+}
