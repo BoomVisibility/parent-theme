87fdf111b7442f7452e25beabef4d6bcf7911aae
BoomVisibility <web@boomvisibility.com>
2019-04-05 12:27:46 -0400

    Update search.php

--- search.php
+++ search.php
@@ -19,7 +19,7 @@ get_header(); ?>
 		
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php get_template_part( 'content', 'search' ); ?>
 			<?php endwhile; ?>
 			<?php wpbeginner_numeric_posts_nav(); ?>
 
