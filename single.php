0efc849bec5f5d560fa67b3df28abc8c72a9de62
BoomVisibility <web@boomvisibility.com>
2019-04-11 10:52:54 -0400

    Clean up code
    Change class from 'category' to 'category-listing' to avoid conflicts with page template.

--- single.php
+++ single.php
@@ -17,30 +17,23 @@ get_header(); ?>
 <div class="wrapper">
 	<div id="primary" class="site-content">
 		<div id="content" class="container small" role="main">
-
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<div class="addthis_sharing_toolbox" data-url="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>"></div>
+			<!-- Go to www.addthis.com/dashboard to customize your tools -->
+			<div class="addthis_sharing_toolbox" data-url="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>"></div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'content', get_post_format() ); ?>
-
-<div class="category"><b><?php _e('Posted In:', 'wplook'); ?></b> <?php the_category(', ') ?><div class="end"></div></div>
-
-<?php if(get_the_tag_list()) {?>
-	<div class="tags"><b><?php _e('Tagged In:', 'wplook'); ?></b> 
-	<?php echo get_the_tag_list('<span>',', ','</span>'); ?><div class="end"></div></div>
-	<?php } ?>
-
-	<?php comments_template( '', true ); ?>
-
-	<?php endwhile; // end of the loop. ?>
-
+				<div class="category-listing">
+					<strong><?php _e('Posted In:', 'wplook'); ?></strong> <?php the_category(', ') ?>
+				</div>
+				<?php if(get_the_tag_list()) {?>
+					<div class="tag-listing">
+						<strong><?php _e('Tagged In:', 'wplook'); ?></strong> <?php echo get_the_tag_list('<span>',', ','</span>'); ?>
+					</div>
+				<?php } ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
-
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
 </div>
-
 <?php get_footer(); ?>
