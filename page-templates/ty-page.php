b06909bbe87a7551c6ab39020289189aad99f772
BoomVisibility <web@boomvisibility.com>
2020-04-23 12:45:15 -0400

    Update
    Standard code added

--- page-templates/ty-page.php
+++ page-templates/ty-page.php
@@ -3,8 +3,8 @@
  * Template Name: Thank You Page
  *
  * @package WordPress
- * @subpackage Bridlebrook
- * @since Bridlebrook 1.0
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
  */
 
 if(!current_user_can('edit_posts')) {
@@ -29,54 +29,24 @@ if(!current_user_can('edit_posts')) {
 }
 
 get_header(); ?>
-<?php if(get_field('header_image')) : ?>
-   <header class="entry-header" style="background-image: url('<?php the_field('header_image'); ?>')">
-     <div class="overlay"></div>
-<?php else : ?>
-  <header class="entry-header" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/header-background.jpg');">
-    <div class="overlay"></div>
-<?php endif; ?>
- 	<div class="container">
-    <div class="textalign-center">
- 		   <h1 class="page-title"><?php the_title(); ?></h1>
-     </div>
-     <div class="clearboth"></div>
- 	</div>
- </header>
+<header class="entry-header">
+	<h1 class="page-title"><?php the_title(); ?></h1>
+</header>
 <div class="wrapper">
-	<div id="content" class="container" role="main">
-    <div id="primary" class="page-content">
+	<div id="primary" class="page-content">
+		<div id="content" class="container" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+					} ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 			<?php endwhile; // end of the loop. ?>
-	   </div><!-- #primary -->
-	<?php get_sidebar(); ?>
-  <?php if(get_field('testimonial')) : ?>
-    <section id="homeposts">
-    <?php
-
-      $post_objects = get_field('testimonial'); ?>
-        <div class="bx-testimonials">
-          <?php
-          foreach ($post_objects as $post) : ?>
-          <?php setup_postdata($post); ?>
-            <div class="featured-testimonial">
-              <div class="one_fourth">
-                <p class="quotes">“</p>
-              </div>
-              <div class="threefourth last">
-                <blockquote>
-                  <?php the_excerpt(); ?>
-                </blockquote>
-              </div>
-              <div class="clearboth"></div>
-              <hr />
-              <h3 class="textalign-center"><?php the_title(); ?></h3>
-              <h5 class="textalign-center"><?php the_field('company'); ?></h5>
-            </div>
-          <?php endforeach; ?>
-      </section>
-  <?php endif; ?>
-	</div><!-- #content -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php if ( is_active_sidebar( 'sidebar-11' ) ) : ?>
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-11' ); ?>				
+		</div><!-- #secondary -->			
+	<?php endif; ?>	
 </div>
 <?php get_footer(); ?>
