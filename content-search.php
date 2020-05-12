f9ca1490b46147ebae236201663c29712905fcb8
BoomVisibility <web@boomvisibility.com>
2020-05-12 12:21:30 -0400

    Update content-search.php

--- content-search.php
+++ content-search.php
@@ -4,8 +4,8 @@
  * The default template for displaying content. Used for both single and index/archive/search.
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Boom Visibility
+ * @since Boom Visibility 1.0
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
