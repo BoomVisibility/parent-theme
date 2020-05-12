637233bc205ef060d5d096889fc182464b909b67
BoomVisibility <web@boomvisibility.com>
2020-05-12 14:53:47 -0400

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * Custom template tags for Twenty Twelve
+ * Custom template tags for Boom Visibility
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Boom Visibility
+ * @since Boom Visibility 1.0
  */
 
-if ( ! function_exists( 'twentytwelve_comment_nav' ) ) :
+if ( ! function_exists( 'boomvisibility_comment_nav' ) ) :
 /**
  * Display navigation to next/previous comments when applicable.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  */
-function twentytwelve_comment_nav() {
+function boomvisibility_comment_nav() {
 	// Are there comments to navigate through?
 	if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
 	?>
 	<nav class="navigation comment-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h2>
+		<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'boomvisibility' ); ?></h2>
 		<div class="nav-links">
 			<?php
-				if ( $prev_link = get_previous_comments_link( __( 'Older Comments', 'twentytwelve' ) ) ) :
+				if ( $prev_link = get_previous_comments_link( __( 'Older Comments', 'boomvisibility' ) ) ) :
 					printf( '<div class="nav-previous">%s</div>', $prev_link );
 				endif;
 
-				if ( $next_link = get_next_comments_link( __( 'Newer Comments', 'twentytwelve' ) ) ) :
+				if ( $next_link = get_next_comments_link( __( 'Newer Comments', 'boomvisibility' ) ) ) :
 					printf( '<div class="nav-next">%s</div>', $next_link );
 				endif;
 			?>
@@ -38,21 +38,21 @@ function twentytwelve_comment_nav() {
 }
 endif;
 
-if ( ! function_exists( 'twentytwelve_entry_meta' ) ) :
+if ( ! function_exists( 'boomvisibility_entry_meta' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  */
-function twentytwelve_entry_meta() {
+function boomvisibility_entry_meta() {
 	if ( is_sticky() && is_home() && ! is_paged() ) {
-		printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'twentytwelve' ) );
+		printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'boomvisibility' ) );
 	}
 
 	$format = get_post_format();
 	if ( current_theme_supports( 'post-formats', $format ) ) {
 		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentytwelve' ) ),
+			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'boomvisibility' ) ),
 			esc_url( get_post_format_link( $format ) ),
 			get_post_format_string( $format )
 		);
@@ -73,7 +73,7 @@ function twentytwelve_entry_meta() {
 		);
 
 		printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
-			_x( 'Posted on', 'Used before publish date.', 'twentytwelve' ),
+			_x( 'Posted on', 'Used before publish date.', 'boomvisibility' ),
 			esc_url( get_permalink() ),
 			$time_string
 		);
@@ -82,24 +82,24 @@ function twentytwelve_entry_meta() {
 	if ( 'post' == get_post_type() ) {
 		if ( is_singular() || is_multi_author() ) {
 			printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				_x( 'Author', 'Used before post author name.', 'twentytwelve' ),
+				_x( 'Author', 'Used before post author name.', 'boomvisibility' ),
 				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 				get_the_author()
 			);
 		}
 
-		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentytwelve' ) );
-		if ( $categories_list && twentytwelve_categorized_blog() ) {
+		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'boomvisibility' ) );
+		if ( $categories_list && boomvisibility_categorized_blog() ) {
 			printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-				_x( 'Categories', 'Used before category names.', 'twentytwelve' ),
+				_x( 'Categories', 'Used before category names.', 'boomvisibility' ),
 				$categories_list
 			);
 		}
 
-		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'twentytwelve' ) );
+		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'boomvisibility' ) );
 		if ( $tags_list ) {
 			printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-				_x( 'Tags', 'Used before tag names.', 'twentytwelve' ),
+				_x( 'Tags', 'Used before tag names.', 'boomvisibility' ),
 				$tags_list
 			);
 		}
@@ -110,7 +110,7 @@ function twentytwelve_entry_meta() {
 		$metadata = wp_get_attachment_metadata();
 
 		printf( '<span class="full-size-link"><span class="screen-reader-text">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
-			_x( 'Full size', 'Used before full size attachment link.', 'twentytwelve' ),
+			_x( 'Full size', 'Used before full size attachment link.', 'boomvisibility' ),
 			esc_url( wp_get_attachment_url() ),
 			$metadata['width'],
 			$metadata['height']
@@ -119,7 +119,7 @@ function twentytwelve_entry_meta() {
 
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
-		comments_popup_link( __( 'Leave a comment', 'twentytwelve' ), __( '1 Comment', 'twentytwelve' ), __( '% Comments', 'twentytwelve' ) );
+		comments_popup_link( __( 'Leave a comment', 'boomvisibility' ), __( '1 Comment', 'boomvisibility' ), __( '% Comments', 'boomvisibility' ) );
 		echo '</span>';
 	}
 }
@@ -128,12 +128,12 @@ endif;
 /**
  * Determine whether blog/site has more than one category.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  *
  * @return bool True of there is more than one category, false otherwise.
  */
-function twentytwelve_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'twentytwelve_categories' ) ) ) {
+function boomvisibility_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'boomvisibility_categories' ) ) ) {
 		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
 			'fields'     => 'ids',
@@ -146,40 +146,40 @@ function twentytwelve_categorized_blog() {
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'twentytwelve_categories', $all_the_cool_cats );
+		set_transient( 'boomvisibility_categories', $all_the_cool_cats );
 	}
 
 	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so twentytwelve_categorized_blog should return true.
+		// This blog has more than 1 category so boomvisibility_categorized_blog should return true.
 		return true;
 	} else {
-		// This blog has only 1 category so twentytwelve_categorized_blog should return false.
+		// This blog has only 1 category so boomvisibility_categorized_blog should return false.
 		return false;
 	}
 }
 
 /**
- * Flush out the transients used in {@see twentytwelve_categorized_blog()}.
+ * Flush out the transients used in {@see boomvisibility_categorized_blog()}.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  */
-function twentytwelve_category_transient_flusher() {
+function boomvisibility_category_transient_flusher() {
 	// Like, beat it. Dig?
-	delete_transient( 'twentytwelve_categories' );
+	delete_transient( 'boomvisibility_categories' );
 }
-add_action( 'edit_category', 'twentytwelve_category_transient_flusher' );
-add_action( 'save_post',     'twentytwelve_category_transient_flusher' );
+add_action( 'edit_category', 'boomvisibility_category_transient_flusher' );
+add_action( 'save_post',     'boomvisibility_category_transient_flusher' );
 
-if ( ! function_exists( 'twentytwelve_post_thumbnail' ) ) :
+if ( ! function_exists( 'boomvisibility_post_thumbnail' ) ) :
 /**
  * Display an optional post thumbnail.
  *
  * Wraps the post thumbnail in an anchor element on index views, or a div
  * element when on single views.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  */
-function twentytwelve_post_thumbnail() {
+function boomvisibility_post_thumbnail() {
 	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 		return;
 	}
@@ -203,40 +203,40 @@ function twentytwelve_post_thumbnail() {
 }
 endif;
 
-if ( ! function_exists( 'twentytwelve_get_link_url' ) ) :
+if ( ! function_exists( 'boomvisibility_get_link_url' ) ) :
 /**
  * Return the post URL.
  *
  * Falls back to the post permalink if no URL is found in the post.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  *
  * @see get_url_in_content()
  *
  * @return string The Link format URL.
  */
-function twentytwelve_get_link_url() {
+function boomvisibility_get_link_url() {
 	$has_url = get_url_in_content( get_the_content() );
 
 	return $has_url ? $has_url : apply_filters( 'the_permalink', get_permalink() );
 }
 endif;
 
-if ( ! function_exists( 'twentytwelve_excerpt_more' ) && ! is_admin() ) :
+if ( ! function_exists( 'boomvisibility_excerpt_more' ) && ! is_admin() ) :
 /**
  * Replaces "[...]" (appended to automatically generated excerpts) with ... and a 'Continue reading' link.
  *
- * @since Twenty Twelve 1.0
+ * @since Boom Visibility 1.0
  *
  * @return string 'Continue reading' link prepended with an ellipsis.
  */
-function twentytwelve_excerpt_more( $more ) {
+function boomvisibility_excerpt_more( $more ) {
 	$link = sprintf( '<a href="%1$s" class="more-link">%2$s</a>',
 		esc_url( get_permalink( get_the_ID() ) ),
 		/* translators: %s: Name of current post */
-		sprintf( __( 'Continue reading %s', 'twentytwelve' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
+		sprintf( __( 'Continue reading %s', 'boomvisibility' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
 		);
 	return ' &hellip; ' . $link;
 }
-add_filter( 'excerpt_more', 'twentytwelve_excerpt_more' );
+add_filter( 'excerpt_more', 'boomvisibility_excerpt_more' );
 endif;
