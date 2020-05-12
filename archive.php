2918f0b85652e88308bec86c541454b586f05c6c
BoomVisibility <web@boomvisibility.com>
2020-05-12 12:16:02 -0400

    Update archive.php

--- archive.php
+++ archive.php
@@ -6,24 +6,24 @@
  * For example, puts together date-based pages if no date.php file exists.
  *
  * If you'd like to further customize these archive views, you may create a
- * new template file for each specific one. For example, Twenty Twelve already
+ * new template file for each specific one. For example, Boom Visibility already
  * has tag.php for Tag archives, category.php for Category archives, and
  * author.php for Author archives.
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage Boom Visibility
+ * @since Boom Visibility 1.0
  */
 get_header(); ?>
 <header class="entry-header">				
 	<h1 class="entry-title">
 		<?php					
-			if ( is_day() ) : printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
-			elseif ( is_month() ) :	printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
-			elseif ( is_year() ) : printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
-			else : _e( 'Archives', 'twentytwelve' );
+			if ( is_day() ) : printf( __( 'Daily Archives: %s', 'boomvisibility' ), '<span>' . get_the_date() . '</span>' );
+			elseif ( is_month() ) :	printf( __( 'Monthly Archives: %s', 'boomvisibility' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'boomvisibility' ) ) . '</span>' );
+			elseif ( is_year() ) : printf( __( 'Yearly Archives: %s', 'boomvisibility' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'boomvisibility' ) ) . '</span>' );
+			else : _e( 'Archives', 'boomvisibility' );
 			endif;
 		?>
 	</h1>
@@ -32,8 +32,6 @@ get_header(); ?>
 	<?php if ( have_posts() ) : ?>			
 	<section id="primary" class="site-content">
 		<div id="content" class="container small" role="main">
-
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
@@ -43,18 +41,13 @@ get_header(); ?>
 				 */
 				get_template_part( 'content', get_post_format() );
 			endwhile;
-
 			wpbeginner_numeric_posts_nav();
 			?>
-
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
-
 		</div><!-- #content -->
-
 	</section><!-- #primary -->
 <?php get_sidebar(); ?>
 </div>
-
 <?php get_footer(); ?>
